91dfec8cff2dbc31fa3d9dd958e8a64d42788c68
benjamin OHANDJA <bendevtech16@gmail.com>
2023-10-09 21:20:49 +0100

    a

---
+++ src/test/java/cm/uy1/entities/ProduitTest.java
@@ -0,0 +1,12 @@
+package cm.uy1.entities;
+
+import org.junit.jupiter.api.Test;
+
+public class ProduitTest {
+    Produit test = new Produit();
+
+    @Test
+    void testGetDesignation() {
+        test.getDesignation();
+    }
+}
