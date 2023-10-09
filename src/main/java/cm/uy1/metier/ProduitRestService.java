5f581256c1caf43c0d8f197798326f1cbe575b39
benjamin OHANDJA <bendevtech16@gmail.com>
2023-10-09 21:08:29 +0100

    @GetMapping

--- src/main/java/cm/uy1/metier/ProduitRestService.java
+++ src/main/java/cm/uy1/metier/ProduitRestService.java
@@ -3,7 +3,6 @@ package cm.uy1.metier;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,16 +14,20 @@ import cm.uy1.entities.Produit;
 public class ProduitRestService {
 	@Autowired
 	private ProduitRepository produitRepository;
-	
+
 	@GetMapping("/produits")
-	List<Produit> produit(){
+	List<Produit> produit() {
 		return produitRepository.findAll();
 	}
-	
+
 	@GetMapping("/produits/{id}")
-	Produit findProduit(@PathVariable Long id ){
+	Produit findProduit(@PathVariable Long id) {
 		return produitRepository.findById(id).get();
 	}
-	
+
+	@GetMapping("/delete/{id}")
+	void deleteProduit(@PathVariable Long id) {
+		produitRepository.deleteById(id);
+	}
 
 }
